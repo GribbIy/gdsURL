10a284a664b5b1f7bdc603c5bca06f59e5c92f83
rockyrebel <99568940+GribbIy@users.noreply.github.com>
2024-10-09 20:53:25 +0100

    Update fetchposts.js

--- fetchposts.js
+++ fetchposts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('posts.json')
+    fetch('API/posts.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
